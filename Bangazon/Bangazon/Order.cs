5dd07d7b3fab39a37dc7c6150248c2aef9fa9f42
Matt Qualls <mattqualls@comcast.net>
2016-05-04 11:33:19 -0500

    Order class setup and properties

---
+++ Bangazon/Bangazon/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bangazon
+{
+     public class Order
+    {
+        public int idOrder { get; set; }
+        public int idCustomer { get; set; }
+        public int idPaymentOption { get; set; }
+        public string shippingMethod { get; set; }
+    }
+}
