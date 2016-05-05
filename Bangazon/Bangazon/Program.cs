3ef52347d710728c9fa2dd0fda84306fbcf372e8
Matt Qualls <mattqualls@comcast.net>
2016-05-05 10:56:38 -0500

    Program class update

--- Bangazon/Bangazon/Program.cs
+++ Bangazon/Bangazon/Program.cs
@@ -10,41 +10,55 @@ namespace Bangazon
     {
         static void Main(string[] args)
         {
-            InvoiceDbInfo db = new InvoiceDbInfo();
-            List<Product> lineItems = new List<Product>();
+            Menu menu = new Menu();
+            List<Product> orderProducts = new List<Product>();
             int menuOption;
+            string outputStr = "";
             bool quitBangazon = false;
 
             while (!quitBangazon)
             {
-                Console.WriteLine("\n******************************************************");
-                Console.WriteLine("** Welcome to Bangazon Command Line Ordering System **");
-                Console.WriteLine("******************************************************\n");
-                Console.WriteLine("1. Create an Account");
-                Console.WriteLine("2. Create a Payment Option");
-                Console.WriteLine("3. Order a Product");
-                Console.WriteLine("4. Complete an Order");
-                Console.WriteLine("5. See Product Popularity");
-                Console.WriteLine("6. Leave Bangazon!\n");
-                Console.Write("Your choice: ");
+                outputStr = menu.displayMainMenu(outputStr);
                 Int32.TryParse(Console.ReadLine(), out menuOption);
+
+                //Console.WriteLine("\n******************************************************");
+                //Console.WriteLine("** Welcome to Bangazon Command Line Ordering System **");
+                //Console.WriteLine("******************************************************\n");
+                //Console.WriteLine("1. Create an Account");
+                //Console.WriteLine("2. Create a Payment Option");
+                //Console.WriteLine("3. Order a Product");
+                //Console.WriteLine("4. Complete an Order");
+                //Console.WriteLine("5. See Product Popularity");
+                //Console.WriteLine("6. Leave Bangazon!\n");
+                //Console.Write("Your choice: ");
+                //Int32.TryParse(Console.ReadLine(), out menuOption);
 
                 switch (menuOption)
                 {
                     case 1: //create an account
+                        menu.addCustomer();
                         break;
                     case 2: //create a payment option
+                        menu.addPaymentOption();
                         break;
                     case 3: //order a product
+                        outputStr = menu.orderProduct(orderProducts);
                         break;
                     case 4: //complete an order
+                        outputStr = menu.completeOrder(orderProducts);
                         break;
                     case 5: //product popularity
+                        menu.popularProducts();
+                        Console.Read();
                         break;
-                    case 6: //Leave Bangazon!
+                    case 6: //Leave Bangazon!!
+                        Console.WriteLine("\nAdios!\n");
+                        quitBangazon = true;
+                        break;
+                    default:
                         break;
                 }
             }
-        }
-    }
-}
+        } //Main
+    } //Program
+}//Namespace
